08c49408d7aa8d796c90bc6ade0908199eb0b22e
huanfs <huanfiguereido@gmail.com>
2025-05-17 15:20:26 -0300

    :sparkles: refactor: renderiza condicionalmente o aviso de 'sem animais cadastrados' ou componente de cadastro de novo animal'

--- src/components/organisms/notDataFound.jsx
+++ src/components/organisms/notDataFound.jsx
@@ -1,21 +1,40 @@
-import React from "react";
+"use client";
+
+import React,{useState} from "react";
 
 import Image from "next/image";
 
+import { FaPlus } from "react-icons/fa6";
+
+import AddPet from "@organisms/addPet";
+
 import {StyledNotDataFound} from "@styles/styledNotDataFound";
 
-import AddButton from "@atoms/addButton";
-
 function NotDataFound(){
+
+    const[isOpenedForm, setIsOpenedForm] = useState(false);
+
     return(
-        <StyledNotDataFound>
-            <Image src="/images/imagem-cachorrinho-triste.png" width="250" height="330" alt="cachorrinho triste"/>
-            <p>
-                Ops, pareçe que você ainda não cadastrou nenhum de seus pets.
-            </p>
-            <AddButton/>
-        </StyledNotDataFound>
+        <>
+            {
+                isOpenedForm ? (
+                    <AddPet action={setIsOpenedForm}/>
+                ) : (
+                    <StyledNotDataFound>
+                        <Image src="/images/imagem-cachorrinho-triste.png" width="250" height="330" alt="cachorrinho triste"/>
+                        <p>
+                            Ops, pareçe que você ainda não cadastrou nenhum de seus pets.
+                        </p>
+                        <button type="button" onClick={()=>{setIsOpenedForm(true)}}>
+                            <FaPlus/>
+                            adicionar
+                        </button>
+                    </StyledNotDataFound>
+                )
+            }
+        </>
     )
 };
 
 export default NotDataFound;
+
--- src/styles/styledNotDataFound.js
+++ src/styles/styledNotDataFound.js
@@ -23,4 +23,31 @@ export const StyledNotDataFound = styled.article`
             text-align:center;
             font-size: var(--Text-size);
         }
+
+        & button{
+            min-width:120px;
+            min-height:120px;
+            border-radius: var(--Rounded);
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            flex-direction:column;
+            gap: var(--Space-short);
+            background-color: var(--Desaturated-yellow);
+            border:none;
+            outline:0;
+            transition: var(--Short);
+            color:var(--White);
+            text-transform:capitalize;
+            font-size: var(--Text-size);
+
+                & svg{
+                    transform:scale(2.2);
+                }
+
+                &:hover{
+                    cursor:pointer;
+                    filter: var(--Lighteen);
+                }
+        }
 `;
