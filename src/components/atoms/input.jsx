2d17f80b730b579ab123bd429a247a0b1dd0b125
huanfs <huanfiguereido@gmail.com>
2025-05-17 15:21:52 -0300

    :construction: refactor: adiciona atributo de nome ao input

--- src/components/atoms/input.jsx
+++ src/components/atoms/input.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 import {StyledInput} from "@styles/styledInput";
 
-function Input({type, placeholder, action}){
+function Input({type, placeholder, action, tag}){
     return(
         <StyledInput
+            name={tag && tag}
             type={type}
             placeholder={placeholder}
             onChange={(event)=>{action && action(event.target.value)}}
