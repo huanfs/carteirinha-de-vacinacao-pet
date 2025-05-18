f2fe84a6ca9aeb88f80b59c81b5c599e5f966edb
huanfs <huanfiguereido@gmail.com>
2025-05-18 19:19:56 -0300

    :construction: refactor: cria l[ogica de renderizar cada vacina a partir do estado

--- src/components/organisms/vaccineList.jsx
+++ src/components/organisms/vaccineList.jsx
@@ -1,15 +1,33 @@
+"use client";
+
 import React from "react";
 
 import AddButton from "@atoms/addButton";
 
+import Vaccine from "@molecules/vaccine";
+
+import useVaccineList from "@store/useVaccineList";
+
 import {StyledVaccineList} from "@styles/styledVaccineList";
 
 function VaccineList(){
+
+    const{vaccineList} = useVaccineList();
+
     return(
         <StyledVaccineList>
             <p>lista de vacinas:</p>
             <article>
                 <AddButton/>
+                {
+                    vaccineList.length > 0 ? (
+                        vaccineList.map((item, index)=>{
+                            return(
+                                <Vaccine vaccineName={item.vaccineName} vaccineDate={item.vaccineDate} key={index}/>
+                            )
+                        })
+                    ) : null
+                }
             </article>
 
         </StyledVaccineList>
