577e567f193bf2eb2bab51d731d06d16f595da4d
huanfs <huanfiguereido@gmail.com>
2025-05-19 12:51:34 -0300

    :sparkles: add: usa jwt para armazenar o id do usuário recém autenticado

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,6 +1,11 @@
 import {Usuarios, Animais} from "@lib/models/modelsReferences";
 
+import jwt from "jsonwebtoken";
+
 export async function POST(request){
+
+    const secretKey = process.env.SECRET_KEY;
+
     try{
         const {name, password} = await request.json();
         const auth = await Usuarios.findOne({
@@ -8,8 +13,11 @@ export async function POST(request){
             include: [{ model: Animais , as: "animais"}],
         });
         if(auth){
+
+            const token = jwt.sign({userId: auth.id}, secretKey, {expiresIn: '1h'})
+
             return new Response(
-                JSON.stringify({message:`Usuário foi encontrado no banco de dados`, animais:auth.animais || []}),{
+                JSON.stringify({message:`Usuário foi encontrado no banco de dados`, animais:auth.animais || [], token}),{
                     status:200,
                     headers:{
                         "Content-Type":"application/json",
