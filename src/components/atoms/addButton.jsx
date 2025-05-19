49de015c6c0a296a4eb03a7b30c31e7b42651731
huanfs <huanfiguereido@gmail.com>
2025-05-19 12:48:09 -0300

    :sparkles: refactor: renderiza corretamente o componente para adicionar nova vacina

--- src/components/atoms/addButton.jsx
+++ src/components/atoms/addButton.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+"use client";
+
+import React,{useState} from "react";
 
 import { FaPlus } from "react-icons/fa6";
+
+import NewVaccine from "@molecules/newVaccine";
 
 import {StyledAddButton} from "@styles/styledAddButton";
 
 function AddButton(){
+
+    const[isOpen, setIsOpen] = useState(false);
+
     return(
-        <StyledAddButton>
-            <FaPlus/>
-            adicionar
-        </StyledAddButton>
+        <>
+        {
+            isOpen ? (
+                <NewVaccine action={setIsOpen}/>
+            ) : (
+                <StyledAddButton onClick={()=>{setIsOpen(true)}}>
+                    <FaPlus/>
+                    adicionar
+                </StyledAddButton>
+            )
+        }
+        </>
     )
 };
 
